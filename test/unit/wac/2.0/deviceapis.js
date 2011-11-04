b26d6ccfd8a31d15f924813883d8b08973e6fbec
Jason Hu <jia-cheng.hu@intel.com>
2011-11-04 10:22:35 +0800

    added geolocation feature to make it a valid feature in wac 2.0  - fixed a few lint complaints on config.js

--- test/unit/wac/2.0/deviceapis.js
+++ test/unit/wac/2.0/deviceapis.js
@@ -22,14 +22,14 @@ describeBrowser("wac_2.0_deviceapis", function () {
         activeCount = 0,
         availCount = 0,
 
-    countAvail = function (objects) {
+    _countAvail = function (objects) {
         var count = 0;
         utils.forEach(objects, function (obj, key) {
             if (obj.feature) {
                 count += obj.feature.split('|').length;
             }
             if (obj.children) {
-                count += countAvail(obj.children);
+                count += _countAvail(obj.children);
             }
         });
         return count;
@@ -51,7 +51,7 @@ describeBrowser("wac_2.0_deviceapis", function () {
         }
         app.setInfo(info);
         activeCount = utils.count(info.features);
-    },
+    };
 
     beforeEach (function () {
         spyOn(platform, "current").andReturn(require('ripple/platform/wac/2.0/spec'));
@@ -61,7 +61,7 @@ describeBrowser("wac_2.0_deviceapis", function () {
     it("listAvailableFeatures should return an array of features", function () {
         var availableFeatures = deviceapis.listAvailableFeatures(),
             spec = require('ripple/platform/wac/2.0/spec');
-        availCount = countAvail(spec.objects);
+        availCount = _countAvail(spec.objects);
         expect(typeof availableFeatures).toBe("object");
         expect(availableFeatures[0].uri).toBe("http://www.w3.org/TR/geolocation-API/");
         expect(typeof availableFeatures.length).toBe("number");
