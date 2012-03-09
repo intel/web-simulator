3ebdbf6809be48d1cead852d82ea734f36e5168f
Jason Hu <jia-cheng.hu@intel.com>
2012-03-09 18:42:32 +0800

    added boolean, range and array filter. enhanced all filters to do 2-tier filtering

--- test/unit/tizen/1.0/tizen1_utils.js
+++ test/unit/tizen/1.0/tizen1_utils.js
@@ -1,24 +1,37 @@
-/*  
+/*
  *  Copyright 2012 Intel Corporation.
- *      
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *          
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- *      
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */ 
+ */
 
 describe("tizen_1.0_tizen1_utils", function () {
     var tutils = require('ripple/platform/tizen/1.0/tizen1_utils');
 
     it("MatchAttributeFilter", function () {
-        var ret,
+        var ret, phones = [],
+            b1 = ["red", "yellow"], b2 = ["yellow", "green"], b3 = ["white", "green"],
+            Brand = function (_color) {
+                return {
+                    color: _color.slice(0)
+                };
+            },
+            Phone = function (_name, _color) {
+                return {
+                    name: _name,
+                    brand: new Brand(_color)
+                };
+            },
+
             dataArray = [{
                 firstName: "Arron",
                 age: 18
@@ -31,7 +44,7 @@ describe("tizen_1.0_tizen1_utils", function () {
                 firstName: "Carson",
                 age: 22
             }],
-            dataSet = { 
+            dataSet = {
                 e1: {
                     firstName: "Alicia",
                     lastName: "Last",
@@ -39,14 +52,26 @@ describe("tizen_1.0_tizen1_utils", function () {
                 },
                 e2: {
                     firstName: "Bianca",
-                    lastName: null, 
+                    lastName: null,
                     age: 9
                 },
                 e3: {
                     firstName: "Celilia",
-                    age: 10 
+                    age: 10
                 }
             };
+
+        phones.push(new Phone("b1", b1));
+        phones.push(new Phone("b2", b2));
+        phones.push(new Phone("b3", b3));
+
+        ret = tutils.matchAttributeArrayFilter(phones, "brand.color", "CONTAINS", "RED");
+        expect(ret.length).toEqual(1);
+        expect(ret[0].name).toEqual("b1");
+        ret = tutils.matchAttributeArrayFilter(phones, "brand.color", "EXACTLY", "yellow");
+        expect(ret.length).toEqual(2);
+        expect(ret[0].name).toEqual("b1");
+        expect(ret[1].name).toEqual("b2");
 
         ret = tutils.matchAttributeFilter(dataArray, "firstName", "EXACTLY", "arron");
         expect(ret.length).toEqual(0);
